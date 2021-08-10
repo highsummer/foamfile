b0ea7c9e05bf8475a57230132580c539969f1f4f
Yoonha Hwang <yoonha.highsummer@gmail.com>
2021-08-11 00:37:14 +0900

    Fix undefined error

--- src/parse/labelList.ts
+++ src/parse/labelList.ts
@@ -31,17 +31,6 @@ export namespace LabelList {
       .desc("ruleHeader")
   }
 
-  function ruleFaceDefinition<Lang extends Language>(lang: Lang): Parser<number[]> {
-    return seq(
-      lang.ruleNumber,
-      word("("),
-      lang.ruleNumber.many(),
-      word(")"),
-    )
-      .map(([length, open, data, close]) => data)
-      .desc("ruleFaceDefinition")
-  }
-
   function ruleDataArray<Lang extends Language>(lang: Lang): Parser<number[]> {
     return seq(
       lang.ruleNumber,
@@ -67,7 +56,6 @@ export namespace LabelList {
     ...Primitive.rules,
     ruleHeaderEntry,
     ruleHeader,
-    ruleFaceDefinition,
     ruleDataArray,
     ruleLabelList,
   }
@@ -78,7 +66,7 @@ export namespace LabelList {
 
   export function parse(input: string): Either<Exception<typeof ParserExceptionCannotParse>, CaseLabelList.Type> {
     try {
-      return right(language.ruleFaceList.tryParse(input))
+      return right(language.ruleLabelList.tryParse(input))
     } catch (e) {
       return left(fail(ParserExceptionCannotParse, e))
     }
